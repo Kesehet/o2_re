90df9da487b3e4c8252b163a6469496dd7d23a00
Kesehet <hamood.siddiqui@gmail.com>
2023-03-04 00:04:20 +0530

    fixed couchdb

--- couchdb.sh
+++ couchdb.sh
@@ -8,3 +8,12 @@ sudo apt install -y couchdb
 sudo systemctl start couchdb
 sudo nano /opt/couchdb/etc/local.ini
 sudo systemctl restart couchdb
+
+
+
+curl -u admin:admin -X PUT http://localhost:8443/users
+curl -u admin:admin -X POST http://localhost:8443/users -H "Content-Type: application/json" -d '{"name":"Hamood Siddiqui", "email":"hamood.siddiqui@gmail.com","AccessIDs":[0,1,2,3],"db":""}'
+
+
+
+
