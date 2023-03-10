6b775a147b72a3ccc7b947cba7387b44dbc639d0
Kesehet <hamood.siddiqui@gmail.com>
2023-03-10 07:28:57 +0530

    create new vm box fixed

--- plugins/azureManager/azureDesign.py
+++ plugins/azureManager/azureDesign.py
@@ -30,6 +30,11 @@ def createNewVMBox():
       <div class="w3-card-4 w3-hover-gray card">
       <i class="fas fa-plus card-icon"></i>
       <h2 style="text-transform:capitalize;" >'''+ "Create New VM" +'''</h2>
+      <p>'''+'''</p>
+          <div class="w3-container">
+              <p>Are you ready to put your virtual carpentry skills to the test?</p>
+              <p>Let's hammer out a new virtual machine and build the future of computing, one bit at a time.</p>
+          </div>
       </div>
     </div>
   '''
