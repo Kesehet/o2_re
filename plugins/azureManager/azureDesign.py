e92e9f3cf7c35a530df60b3fc23b3aeb657c99b4
Kesehet <hamood.siddiqui@gmail.com>
2023-03-10 08:06:38 +0530

    overflow fix

--- plugins/azureManager/azureDesign.py
+++ plugins/azureManager/azureDesign.py
@@ -26,7 +26,7 @@ def VMsBox():
   return ret
 def createNewVMBox():
   return '''
-      <div onclick="window.location.href = 'create_vm?step=1'" class="w3-container w3-third w3-padding zoomOnHover w3-tooltip">  
+      <div onclick="window.location.href = 'create_vm?step=1'" class="w3-container w3-third w3-padding zoomOnHover w3-tooltip" style="overflow:hidden;">
       <div class="w3-card-4 w3-hover-gray card">
       <i class="fas fa-plus card-icon"></i>
       <h2 style="text-transform:capitalize;" >'''+ "Create New VM" +'''</h2>
