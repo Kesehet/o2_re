b61277d6b28fa5b3b3978e251ab685b58ba815d4
Kesehet <hamood.siddiqui@gmail.com>
2023-03-11 16:52:45 +0530

    shell_runner added

--- plugins/shell_runner/shell_runner.py
+++ plugins/shell_runner/shell_runner.py
@@ -1,12 +1,11 @@
-name = "subtractor"
-description = "I am a simple subtraction plugin that calculates the difference between two numbers."
-fa_icon = "minus"
+name = "shell_runner"
+description = ""
+fa_icon = "terminal"
 privLevel = 1
 publicLinks = ["main", "mresult"]
 
 from funcs import settings as S
 from funcs import shell as SHELL
-import subprocess
 
 def main(request):
   return template("")
