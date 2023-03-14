5678be0799ee94c1225518f69cf4fb94b5d20560
Kesehet <hamood.siddiqui@gmail.com>
2023-03-14 21:10:28 +0530

    ..

--- plugins/MOUS_azureManager/MOUS_azureManager.py
+++ plugins/MOUS_azureManager/MOUS_azureManager.py
@@ -20,27 +20,27 @@ def mresult(request):
 def template(result):
   return '''
   <div class="w3-container">
-    <h1 class="w3-tooltip">
-      <img src="https://icons8.com/icon/vqF20UXnIgtz/rat" style="width:100%;"> 
+    <div class="w3-tooltip">
       Azure Manager
-    </h1>
-    <div class="w3-text w3-tiny">
-      Yes, as an Azure Cloud Manager, I am capable of managing all your machines in Azure including creating, deleting, and load balancing them using Azure PowerShell. Additionally, I can provide you with analytics on all the machines to help you make informed decisions about your infrastructure.
+    <div class="w3-container w3-text w3-tiny">
+        Yes, as an Azure Cloud Manager, I am capable of managing all your machines in Azure including creating, deleting, and load balancing them using Azure PowerShell. Additionally, I can provide you with analytics on all the machines to help you make informed decisions about your infrastructure.
 
-      Here are some of the tasks I can perform for you using Azure PowerShell:
+        Here are some of the tasks I can perform for you using Azure PowerShell:
 
-      Create virtual machines: I can use Azure PowerShell to create virtual machines for you in Azure.
+        Create virtual machines: I can use Azure PowerShell to create virtual machines for you in Azure.
 
-      Delete virtual machines: If you want to delete virtual machines, I can use Azure PowerShell to do it for you.
+        Delete virtual machines: If you want to delete virtual machines, I can use Azure PowerShell to do it for you.
 
-      Load balancing: I can configure load balancing for your virtual machines using Azure PowerShell. This will ensure that your applications remain available even if one or more virtual machines fail.
+        Load balancing: I can configure load balancing for your virtual machines using Azure PowerShell. This will ensure that your applications remain available even if one or more virtual machines fail.
 
-      Monitor machines: I can use Azure PowerShell to monitor the performance of your virtual machines, including CPU usage, memory usage, and disk space.
+        Monitor machines: I can use Azure PowerShell to monitor the performance of your virtual machines, including CPU usage, memory usage, and disk space.
 
-      Analytics: I can provide you with detailed analytics on your virtual machines, including usage patterns, performance trends, and resource consumption. This information can help you optimize your infrastructure and reduce costs.
+        Analytics: I can provide you with detailed analytics on your virtual machines, including usage patterns, performance trends, and resource consumption. This information can help you optimize your infrastructure and reduce costs.
 
-      Overall, as your Azure cloud manager, I will ensure that your infrastructure runs smoothly, and I will provide you with the insights you need to make informed decisions about your infrastructure.
+        Overall, as your Azure cloud manager, I will ensure that your infrastructure runs smoothly, and I will provide you with the insights you need to make informed decisions about your infrastructure.
     </div>
+    </div>
+    
     ''' + str(result) + '''
   </div>
   '''
