4295e762f7c7223f780199bdc437432a45c5e4ff
Kesehet <hamood.siddiqui@gmail.com>
2023-03-30 20:20:14 +0530

    asdasd

--- api/MOUS_azureManager/MOUS_azureFunctions.py
+++ api/MOUS_azureManager/MOUS_azureFunctions.py
@@ -16,7 +16,7 @@ def main():
         
         data = doc.get("value", {}).get("data",{})
         
-        resource_group_name = data.get("group")+azure_vm_manager.generate_random_string(12)
+        resource_group_name = data.get("group")+"-"+azure_vm_manager.generate_random_string(12)
         vm_name = data.get("vm_name")
         location = data.get("location")
         size = data.get("tier")
@@ -78,7 +78,7 @@ class AzureVmManager:
         size = self.get_best_size(location=location, image_type=size.lower())
             
         # Create the VM using the selected size
-        command = f"""New-AzVm -ResourceGroupName {resource_group_name} -Name {vm_name} -Location {location} -Size {size} -PublicIpSku Standard -Zone 1 -Image {image} -Credential (New-Object System.Management.Automation.PSCredential('{username}', (ConvertTo-SecureString '{password}' -AsPlainText -Force)))"""
+        command = f"""New-AzVm -ResourceGroupName {resource_group_name} -Name {vm_name} -Location {location} -Size {size} -PublicIpSku Standard -Image {image} -Credential (New-Object System.Management.Automation.PSCredential('{username}', (ConvertTo-SecureString '{password}' -AsPlainText -Force)))"""
         print(command)
         result = self.run_powershell_command(command)
 	
@@ -207,5 +207,5 @@ Update-AzVM -ResourceGroupName {resource_group_name} -VM $vm"""
         import random
         import string
         random_string = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
-        return f"-{random_string}-{''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))}"
+        return f"{random_string}-{''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))}"
 
